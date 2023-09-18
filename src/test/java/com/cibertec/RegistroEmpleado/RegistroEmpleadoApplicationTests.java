4260e9b9606cc07737ba15dd1e4a346696ded184
DisneyMedina <disney.medina27@gmail.com>
2023-09-17 20:16:35 -0500

    Proyecto inicial

---
+++ src/test/java/com/cibertec/RegistroEmpleado/RegistroEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.RegistroEmpleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
